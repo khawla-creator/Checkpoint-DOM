ecf29df0932ef91c0a9a4087c3db5c832720bc93
khawla guesmi <khawlaguesmi712@gmail.com>
2021-05-31 11:56:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+window. alert('Hello , welcom in im shopping cart'); // [1] l'alert s'affiche dans les deux pages 
+
+var mydiv = document.getElementById('besoinAide');  // [2]  chnagement dans la page html 2 
+
+mydiv.innerHTML= 'Votre choix est le meilleur, merci !!' ; // [2] innerHTML change le contenue de ID
+
+ 
+var btn-plus=document.getElementsByClassName("ajouter-fleur") //
+
+var counter=document.getElementsByClassName("conteneur-1-")
+
+var btn-moin=document.getElementsByClassName("ignorer-fleur")
+
+
+
+
+
+
+
+
+
+
+
